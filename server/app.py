6962b21bc27b56d13c39991e331e3c2158f9e4dd
Yusef Codes <youcefcopy@gmail.com>
2026-04-08 16:33:46 +0100

    imports and loading the keras model

--- server/app.py
+++ server/app.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS          # ← add this
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+from PIL import Image
+import io
 
 app = Flask(__name__)
 CORS(app)
+
+# Load your model once
+model = load_model('model/cnn_xray_v1.keras')  # or .h5
 
 @app.route('/predict', methods=['GET'])
 def send_prediction():
