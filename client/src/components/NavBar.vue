ff99ab99c05816acb1edc975b2ffc5fe64abfdf1
Yusef Codes <youcefcopy@gmail.com>
2026-04-08 14:47:40 +0100

    styling the home page

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -4,7 +4,7 @@
 
 <template>
   <nav class="flex justify-start px-4 py-2">
-    <h1 class="text-blue-400 text-7xl">Pneumo Scan</h1>
+    <h1 class="text-blue-400 text-3xl">Pneumo Scan</h1>
   </nav>
 </template>
 
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -3,8 +3,10 @@
 </script>
 
 <template>
-  <div>
-    <h1 class="text-blue-400">Hey man</h1>
+  <div class="flex flex-col justify-center h-100 bg-[#F8FAFC]">
+    <h1 class="text-4xl my-2">AI-powered Chest X-ray Classification</h1>
+    <p class="my-2">Upload an X-ray image and get instant predictions.</p>
+    <button class="px-2 py-1 mx-auto w-20 bg-[#2563EB] hover:bg-[#366bde] text-white cursor-pointer border rounded-2">Predict</button>
   </div>
 </template>
 
