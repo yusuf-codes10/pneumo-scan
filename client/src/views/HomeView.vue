6f2741edd8f26682be2418de3bad6b848ebffe17
Yusef Codes <youcefcopy@gmail.com>
2026-04-08 15:09:34 +0100

    the hero section is looking cleaner for now

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -3,11 +3,11 @@
 
 <template>
   <div class="flex flex-col justify-center h-100 ">
-    <div class="w-200 bg-red-600">
-      <h1 class="text-4xl my-2">AI-powered Chest X-ray Classification</h1>
+    <div class="flex flex-col justify-center items-center w-150">
+      <h1 class="text-3xl my-2">AI-powered Chest X-ray Classification</h1>
       <p class="my-2">Upload an X-ray image and get instant predictions.</p>
       <button
-        class="px-2 py-1 mx-auto w-20 bg-[#2563EB] hover:bg-[#366bde] text-white cursor-pointer border rounded-2"
+        class="block px-2 py-1 mx-auto w-20 bg-[#2563EB] hover:bg-[#366bde] text-white cursor-pointer border rounded-2"
       >
         Predict
       </button>
