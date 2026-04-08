ec788c215e8d58ebd27c041cdd5a161068e0fc6a
Yusef Codes <youcefcopy@gmail.com>
2026-04-08 15:36:18 +0100

    preparing the first get request in flask

--- server/app.py
+++ server/app.py
@@ -2,4 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.run(debug=True)
+@app.route('/predict', methods=['GET'])
+def send_prediction():
+    pass
+
+if __name__ in "__main__":
+    app.run(debug=True)
