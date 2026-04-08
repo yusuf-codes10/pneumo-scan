dace3738f659cef861df89a67b50626533eb1579
Yusef Codes <youcefcopy@gmail.com>
2026-04-08 15:17:18 +0100

    styling more styling

--- client/src/views/ClassifyView.vue
+++ client/src/views/ClassifyView.vue
@@ -4,7 +4,9 @@
 
 <template>
   <div>
-
+    <div class="w-100 h-100 bg-[#F8FAFC] shadow-2xs">
+      <button class="block px-2 py-1 mx-auto  bg-[#2563EB] hover:bg-[#366bde] text-white cursor-pointer border rounded-2">Upload image</button>
+    </div>
   </div>
 </template>
 
